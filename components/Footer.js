7cc4f4cd30fee1a92e37863b42cabac07fe934ed
jiyoungv <jiyoungvvvvv@gmail.com>
2021-02-15 21:58:45 +0900

    Add link from Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,8 @@ function Footer () {
     return (
         <FooterLayout>
             <div>
-                <a href="https://github.com/jiyoungv/nextjs-simple-todo" target="_blank" rel="noreferrer noopener" title="Go to Github">Go to Github</a>
+                <p><a href="https://github.com/jiyoungv/nextjs-simple-todo" target="_blank" rel="noreferrer noopener" title="Go to Github">Github 바로가기</a></p>
+                <p><a href="https://blog.naver.com/pkjyng/222244701061" target="_blank" rel="noreferrer noopener" title="작업일지">작업일지 바로가기</a></p>
                 <h6>ⓒ 2020 PARK JI YOUNG.</h6>
             </div>
         </FooterLayout>
