ab1e8de145d6c4e07dc672f79ff7edaf0bf6c88e
박지영 <63575402+jiyoungv@users.noreply.github.com>
2022-01-08 21:49:24 +0900

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,6 @@ function Footer () {
         <FooterLayout>
             <div>
                 <p><a href="https://github.com/jiyoungv/nextjs-simple-todo" target="_blank" rel="noreferrer noopener" title="Go to Github">Github 바로가기</a></p>
-                <p><a href="https://blog.naver.com/pkjyng/222244701061" target="_blank" rel="noreferrer noopener" title="작업일지">작업일지 바로가기</a></p>
                 <h6>ⓒ 2020 PARK JI YOUNG.</h6>
             </div>
         </FooterLayout>
